3d1b0450fbd188ee5497a93d092aacd06898af9b
Jeevan Prakash <jeevan.prakash@cognitivemachines.co.in>
2017-09-27 10:34:09 +0530

    opencv & matplotlib

--- opencv_matplotlib.py
+++ opencv_matplotlib.py
@@ -1,18 +1,21 @@
 import cv2
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('/home/jps/Pictures/lena_std.tif')
-img2 = cv2.imread('/home/jps/Pictures/opencv_logo.jpg')
-imgg1 = cv2.imread('/home/jps/Pictures/lena_std.tif', 0)
-imgg2 = cv2.imread('/home/jps/Pictures/opencv_logo.jpg', 0)
+
+if __name__ == '__main__':
+
+    img1 = cv2.imread('/home/jps/Pictures/lena_std.tif')
+    img2 = cv2.imread('/home/jps/Pictures/opencv_logo.jpg')
+    imgg1 = cv2.imread('/home/jps/Pictures/lena_std.tif', 0)
+    imgg2 = cv2.imread('/home/jps/Pictures/opencv_logo.jpg', 0)
 
 
-plt.subplot(221),plt.imshow(img1),plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(222),plt.imshow(imgg1),plt.title('Averaging')
-plt.xticks([]), plt.yticks([])
-plt.subplot(223),plt.imshow(img2),plt.title('Averaging')
-plt.xticks([]), plt.yticks([])
-plt.subplot(224),plt.imshow(imgg2),plt.title('Averaging')
-plt.xticks([]), plt.yticks([])
-plt.show()
+    plt.subplot(221),plt.imshow(img1),plt.title('Original')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(222),plt.imshow(imgg1),plt.title('Averaging')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(223),plt.imshow(img2),plt.title('Averaging')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(224),plt.imshow(imgg2),plt.title('Averaging')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
