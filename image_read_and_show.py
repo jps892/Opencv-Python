c7d034c8709acdd363fa586c04286fc15f12e078
Jeevan Prakash <jeevan.prakash@cognitivemachines.co.in>
2017-09-27 09:11:21 +0530

    image read, write and show

--- image_read_and_show.py
+++ image_read_and_show.py
@@ -4,9 +4,13 @@ PATH_NAME = '/home/jps/Pictures/resized_H/h_pipes_118.jpg.jpg'
 
 
 if __name__ == '__main__':
+    #read
     img = cv2.imread(PATH_NAME)
 
+    #write
     cv2.imwrite('/home/jps/H_pile.png', img)
+
+    #show
     cv2.imshow("Contour", img)
     k = cv2.waitKey(0)
     if k == 27:
